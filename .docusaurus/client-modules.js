a0ea641dccbe3bdcfd8c372e5c2a860c302c126c
gorpaseth <betta@duck.com>
2024-10-18 10:58:28 +0300

    Rebuild and push site

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/gorpaseth/Downloads/eduardg-cv/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/gorpaseth/Downloads/eduardg-cv/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/gorpaseth/Downloads/eduardg-cv/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/gorpaseth/Downloads/eduardg-cv/src/css/custom.css"),
+];
